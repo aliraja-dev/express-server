3b36d0f28bf4e4d2df8d34e97238935a358855b3
Ali Raja <aliraja.en@gmail.com>
2022-08-22 14:29:46 -0400

    Added: DOTENV for ENV variables

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://root-admin:tO355OK63njKULeW@cluster0.ai9zw.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.ATLAS_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -7,10 +7,10 @@ var _db;
 
 module.exports = {
     connectToServer: function (callback) {
-        client.connect(function (err, db) {
+        client.connect(function (err, uri) {
             // Verify we got a good "db" object
-            if (db) {
-                _db = db.db("AngularDB");
+            if (uri) {
+                _db = uri.db("AngularDB");
                 console.log("Successfully connected to MongoDB.");
             }
             return callback(err);
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+require("dotenv").config();
+console.log(process.env);
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
